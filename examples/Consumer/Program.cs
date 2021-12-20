6a20048e8a4beb0fd0bbca82dd99e44ab94e3ece
ydy <yang.dengyong@yalla.live>
2021-12-20 09:46:13 +0800

    feat:add batch test demo

--- examples/Consumer/Program.cs
+++ examples/Consumer/Program.cs
@@ -78,6 +78,102 @@ namespace Confluent.Kafka.Examples.ConsumerExample
 
                 try
                 {
+                   
+                    while (true)
+                    {
+                        try
+                        {
+                            var consumeResult = consumer.Consume(cancellationToken);
+
+                            if (consumeResult.IsPartitionEOF)
+                            {
+                                Console.WriteLine(
+                                    $"Reached end of topic {consumeResult.Topic}, partition {consumeResult.Partition}, offset {consumeResult.Offset}.");
+
+                                continue;
+                            }
+
+                            Console.WriteLine($"Received message at {consumeResult.TopicPartitionOffset}: {consumeResult.Value}");
+
+                            if (consumeResult.Offset % commitPeriod == 0)
+                            {
+                                // The Commit method sends a "commit offsets" request to the Kafka
+                                // cluster and synchronously waits for the response. This is very
+                                // slow compared to the rate at which the consumer is capable of
+                                // consuming messages. A high performance application will typically
+                                // commit offsets relatively infrequently and be designed handle
+                                // duplicate messages in the event of failure.
+                                try
+                                {
+                                    consumer.Commit(consumeResult);
+                                }
+                                catch (KafkaException e)
+                                {
+                                    Console.WriteLine($"Commit error: {e.Error.Reason}");
+                                }
+                            }
+                        }
+                        catch (ConsumeException e)
+                        {
+                            Console.WriteLine($"Consume error: {e.Error.Reason}");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    Console.WriteLine("Closing consumer.");
+                    consumer.Close();
+                }
+            }
+        }
+
+        /// <summary>
+        ///     In this example
+        ///         - offsets are manually committed.
+        ///         - no extra thread is created for the Poll (Consume) loop.
+        /// </summary>
+        public static void Run_ConsumeBatch(string brokerList, List<string> topics, CancellationToken cancellationToken)
+        {
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = brokerList,
+                GroupId = "csharp-consumer",
+                EnableAutoCommit = false,
+                StatisticsIntervalMs = 5000,
+                SessionTimeoutMs = 6000,
+                AutoOffsetReset = AutoOffsetReset.Earliest,
+                EnablePartitionEof = true
+            };
+
+            const int commitPeriod = 5;
+
+            // Note: If a key or value deserializer is not set (as is the case below), the 
+            // deserializer corresponding to the appropriate type from Confluent.Kafka.Deserializers
+            // will be used automatically (where available). The default deserializer for string
+            // is UTF8. The default deserializer for Ignore returns null for all input data
+            // (including non-null data).
+            using (var consumer = new ConsumerBuilder<Ignore, string>(config)
+                // Note: All handlers are called on the main .Consume thread.
+                .SetErrorHandler((_, e) => Console.WriteLine($"Error: {e.Reason}"))
+                // .SetStatisticsHandler((_, json) => Console.WriteLine($"Statistics: {json}"))
+                .SetPartitionsAssignedHandler((c, partitions) =>
+                {
+                    Console.WriteLine($"Assigned partitions: [{string.Join(", ", partitions)}]");
+                    // possibly manually specify start offsets or override the partition assignment provided by
+                    // the consumer group by returning a list of topic/partition/offsets to assign to, e.g.:
+                    // 
+                    // return partitions.Select(tp => new TopicPartitionOffset(tp, externalOffsets[tp]));
+                })
+                .SetPartitionsRevokedHandler((c, partitions) =>
+                {
+                    Console.WriteLine($"Revoking assignment: [{string.Join(", ", partitions)}]");
+                })
+                .Build())
+            {
+                consumer.Subscribe(topics);
+
+                try
+                {
                     while (true)
                     {
                         try
@@ -93,7 +189,8 @@ namespace Confluent.Kafka.Examples.ConsumerExample
 
                                     commitResult = consumeResult;
                                 }
-                            }catch(KafkaException e)
+                            }
+                            catch (KafkaException e)
                             {
                                 Console.WriteLine($"Commit error: {e.Error.Reason}");
                             }
@@ -103,11 +200,11 @@ namespace Confluent.Kafka.Examples.ConsumerExample
                             }
                             finally
                             {
-                                if(commitResult != null)
+                                if (commitResult != null)
                                 {
                                     consumer.Commit(commitResult);
                                 }
-                            }                         
+                            }
 
                         }
                         catch (ConsumeException e)
@@ -165,6 +262,7 @@ namespace Confluent.Kafka.Examples.ConsumerExample
                 }
             }
         }
+
 
         /// <summary>
         ///     In this example
@@ -224,15 +322,15 @@ namespace Confluent.Kafka.Examples.ConsumerExample
 
         public static void Main(string[] args)
         {
-            //if (args.Length < 3)
-            //{
-            //    PrintUsage();
-            //    return;
-            //}
+            if (args.Length < 3)
+            {
+                PrintUsage();
+                return;
+            }
 
-            var mode = "subscribe";// args[0];
-            var brokerList = "172.28.32.167:9092";// args[1];
-            var topics = new List<string>() { "test" };//args.Skip(2).ToList();
+            var mode = args[0];
+            var brokerList =  args[1];
+            var topics = args.Skip(2).ToList();
 
             Console.WriteLine($"Started consumer, Ctrl-C to stop consuming");
 
@@ -249,6 +347,9 @@ namespace Confluent.Kafka.Examples.ConsumerExample
                     break;
                 case "manual":
                     Run_ManualAssign(brokerList, topics, cts.Token);
+                    break;
+                case "subscribeBatch":
+                    Run_ConsumeBatch(brokerList, topics, cts.Token);
                     break;
                 default:
                     PrintUsage();
